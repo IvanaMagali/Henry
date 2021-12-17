90f88ead4f16b73056aec1aece4c1fd3f2605965
Magui <ivanamagalit@hotmail.com>
2021-12-17 11:22:14 -0300

    Subiendo tarea

---
+++ 02 JS I homework teorico.js
@@ -0,0 +1,18 @@
+Variable: Es el nombre que se le da a una "caja" donde se almacenan valores.
+          Los valores guardados en esa caja se pueden modificar durante el uso de la misma.
+          Deben "mencionarse" (declararse) antes de ser usadas.
+
+String: Es un tipo de dato representado por una cadena de caracteres.
+        Siempre van entre comillas "" ''.
+
+Booleanos: Es un tipo de dato que solo admite un resultado: True/False
+
+Argumento: Son valores que se le da a una función cuando se la menciona/invoca.
+           Los argumentos se escriben entre paréntesis.
+
+Return: Es una instrucción que se da dentro de una función, y devuelve
+        el valor, el resultado de la misma.
+
+If: Es una sentencia que evalúa la condición de un paréntesis dentro de la función.
+    Si esta condición se cumple, devuelve un valor booleano o una string.
+
